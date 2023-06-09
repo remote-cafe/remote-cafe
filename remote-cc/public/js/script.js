52d7e34946c94b6e1b32a52553661d470af479c8
disaacjm <disaacjm@gmail.com>
2023-06-09 16:47:17 +0100

    initial setup

---
+++ remote-cc/public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("remote-cc JS imported successfully!");
+});
